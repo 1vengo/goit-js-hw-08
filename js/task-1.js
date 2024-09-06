b4d6f6e865bdeea792b5b90a3466182b61554701
Ivan <159075250+1vengo@users.noreply.github.com>
2024-09-06 16:14:43 +0200

    add

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const elements = document.querySelectorAll('#categories .item');
-console.log('Number of categories:', elements.length);
-elements.forEach(element => {
-    const title = element.querySelector('h2').textContent;
-    const name = element.querySelectorAll('li');
-    console.log('Category:', title);
-    console.log('Elements:', name.length)
-});
